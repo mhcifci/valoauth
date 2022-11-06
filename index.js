8a81e9cded9807cb538d83616f8dd33354c4ba4e
mhcifci <mhcifci@gmail.com>
2022-11-06 19:33:55 +0300

    dynamic login fixed, delete old code
    dynamic login fixed, delete old code

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
 const express = require('express');
 const app = express();
 
-
-/* My routes */
-
 // For access to JWT and Authorization
-const Userroute  = require("./routes/Userroute");
-// For fetching Valorant data and many more.
-const fetch  = require("./routes/fetch");
-
+const UserRoute  = require("./routes/UserRoute");
 
 const PORT = 3000;
 app.listen(PORT, () => {});
 
-
-app.use('/user', Userroute);
-app.use('/fetch', fetch);
+app.use('/user', UserRoute);
--- routes/Userroute.js
+++ routes/Userroute.js
@@ -3,27 +3,42 @@ const router = express.Router();
 const Valorant = require('@liamcottle/valorant.js');
 const valorantApi = new Valorant.API(Valorant.Regions.Europe);
 
-router.get("/login", (req, res) => {
+let bodyParser = require('body-parser');
+const { json } = require('express');
+router.use(bodyParser.json());
 
-    let userData =  [];
-    valorantApi.authorize('mahmutcannx', '2146422Mahmut').then(() => {
+router.post("/login", (req, res) => {
+
+    console.log ( req.body );   
+
+    const id = req.body.id;
+    const pass = req.body.pass;
+
+    if (id == undefined || pass === undefined) {
+        res.status(400).json({
+            "status": "error",
+            "message": "Please fill your ID and Password!"
+        });
+        return false;
+    }
+    valorantApi.authorize(id, pass).then(() => {
         data = {
             "user_id": valorantApi.user_id,
             "access_token": valorantApi.access_token,
             "entitlements_token": valorantApi.entitlements_token,
         }
-        userData.push(data);
         res.status(200).json({
             "status": "success",
-            "data": userData
+            "data": data
         });
     }).catch((error) => {
         res.status(400).json({
             "status": "error",
             "message": "Bad request!",
-            "data"  : error
+            "data": error
         });
     });
+
 });
 
-module.exports = router ;
+module.exports = router;
