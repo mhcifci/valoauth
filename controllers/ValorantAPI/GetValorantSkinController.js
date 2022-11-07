688017080ace808ac6f480577bf7431e880d53ba
Overcodex Dev <overcodexdev@Overcodex-MacBook-Air.local>
2022-11-07 09:55:59 +0300

    update valorantapi folder

--- controllers/ValorantAPI/GetValorantSkinController.js
+++ controllers/ValorantAPI/GetValorantSkinController.js
@@ -1,32 +1,20 @@
-const axios = require('axios');
-
-
-
-
-const fetchSkin = async (skins) => {
-
-    await skins.map(item => {
-        axios.get("https://valorant-api.com/v1/weapons/skinlevels/" + item)
-            .then((response) => {
-                console.log(response.data);
-            }).catch((err) => {
-                return err;
-            });
-    });
-}
+const axios = require("axios");
 
 const GetValorantSkinController = async (req, res) => {
-    const skins = req.body.skins;
-    if (!skins) {
-        res.status(403).json({
-            "status": "error",
-            "message": "Please type your Skin UUID"
-        });
-    }
-    const data = await fetchSkin(skins);
+  const skins = req.body.skins;
 
+  if (typeof skins === "object") {
+    let skinArr = [];
+    for (let index = 0; index < skins.length; index++) {
+      let result = await axios.get(
+        "https://valorant-api.com/v1/weapons/skinlevels/" + skins[index]
+      );
+      skinArr.push(result.data);
+    }
+    res.status(200).json(skinArr);
+  }
 };
 
 module.exports = {
-    GetValorantSkinController
-}
+  GetValorantSkinController,
+};
