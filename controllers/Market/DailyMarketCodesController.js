8c5c7a56f3d48b30f965a2a7597f843b2d7fb142
Mahmut Can ÇİFÇİ <mhcifci@gmail.com>
2022-11-17 23:25:15 +0300

    api.js changed to exact valorant vers.

--- controllers/Market/DailyMarketCodesController.js
+++ controllers/Market/DailyMarketCodesController.js
@@ -1,40 +1,58 @@
-const axios = require('axios');
-const { restart } = require('nodemon');
+const axios = require("axios");
+const { restart } = require("nodemon");
 
-const DailyMarketCodes = async (req , res) => {
-
-    const userId = req.body.user_id;
-    const accessToken = req.body.access_token;
-    const entitlementsToken = req.body.entitlements_token;
-
-    const baseUrl = "https://pd.eu.a.pvp.net/store/v2/";
-    const useMethod = "storefront/";
-
-    if (!userId || !accessToken || !entitlementsToken) {
-        res.status(418).json({
-            "status": "error",
-            "message": "Blap blap cort, isteğini bir kontrol et la!"
-        });
+async function getValorantSkins(skins) {
+  if (typeof skins === "object") {
+    let skinArr = [];
+    for (let index = 0; index < skins.length; index++) {
+      let result = await axios.get(
+        "https://valorant-api.com/v1/weapons/skinlevels/" + skins[index]
+      );
+      skinArr.push(result.data.data);
     }
-    await axios.get(baseUrl + useMethod + userId, {
-        headers: {
-            'Content-Type': 'application/json',
-            'X-Riot-Entitlements-JWT': entitlementsToken,
-            'Authorization': 'Bearer ' + accessToken
-        }
-    }).then((result) => {
+    return skinArr;
+  }
+}
+
+const DailyMarketCodes = async (req, res) => {
+  const userId = req.body.user_id;
+  const accessToken = req.body.access_token;
+  const entitlementsToken = req.body.entitlements_token;
+
+  const baseUrl = "https://pd.eu.a.pvp.net/store/v2/";
+  const useMethod = "storefront/";
+
+  if (!userId || !accessToken || !entitlementsToken) {
+    res.status(418).json({
+      status: "error",
+      message: "Blap blap cort, isteğini bir kontrol et la!",
+    });
+  }
+  await axios
+    .get(baseUrl + useMethod + userId, {
+      headers: {
+        "Content-Type": "application/json",
+        "X-Riot-Entitlements-JWT": entitlementsToken,
+        Authorization: "Bearer " + accessToken,
+      },
+    })
+    .then((result) => {
+      const skins = result.data.SkinsPanelLayout.SingleItemOffers;
+      getValorantSkins(skins).then((result) => {
         res.status(200).json({
-            "status": "success",
-            "data": result.data.SkinsPanelLayout.SingleItemOffers
+          status: "success",
+          data: result
         });
-    }).catch((err) => {
-        res.status(403).json({
-            "status": "error",
-            "data"  : err.message
-        });
+      });
+    })
+    .catch((err) => {
+      res.status(403).json({
+        status: "error",
+        data: err.message,
+      });
     });
 };
 
 module.exports = {
-    DailyMarketCodes
-}
+  DailyMarketCodes,
+};
