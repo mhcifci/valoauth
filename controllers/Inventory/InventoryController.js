1d067deb0a971a06e5d192e9b239df48a5b00ce6
Mahmut Can ÇİFÇİ <mhcifci@gmail.com>
2022-12-04 03:31:52 +0300

    inventory response edited

--- controllers/Inventory/InventoryController.js
+++ controllers/Inventory/InventoryController.js
@@ -1,14 +1,41 @@
 const axios = require("axios");
 const { restart } = require("nodemon");
 
+async function getValorantWeapons(skins, lang = "en-US") {
+  if (typeof skins === "object") {
+    let skinArr = [];
+    for (var i = 0; i < skins.length; i++) {
+      let result = await axios.get(
+        "https://valorant-api.com/v1/weapons/skins/" + skins[i].SkinID + "?language="+lang
+      );
+      skinArr.push(result.data.data);
+    }
+    return skinArr;
+  }
+}
+
+async function getValorantSprays(skins, lang = "en-US") {
+  if (typeof skins === "object") {
+    let skinArr = [];
+    for (var i = 0; i < skins.length; i++) {
+      let result = await axios.get(
+        "https://valorant-api.com/v1/sprays/" + skins[i].SprayID + "?language="+lang
+      );
+      skinArr.push(result.data.data);
+    }
+    return skinArr;
+  }
+}
+
 const InventoryController = async (req, res) => {
   const userId = req.body.user_id;
   const accessToken = req.body.access_token;
   const entitlementsToken = req.body.entitlements_token;
   const region = req.body.region;
+  const lang = req.body.lang;
 
   const baseUrl = "https://pd." + region + ".a.pvp.net/";
-  const useMethod = "personalization/v2/players/"+userId+"/playerloadout";
+  const useMethod = "personalization/v2/players/" + userId + "/playerloadout";
 
   if (!userId || !accessToken || !entitlementsToken || !region) {
     res.status(418).json({
@@ -24,11 +51,21 @@ const InventoryController = async (req, res) => {
         Authorization: "Bearer " + accessToken,
       },
     })
-    .then((result) => {
-      console.log(result);
+    .then(async (result) => {
+      const weapons = [];
+      const sprays = [];
+      await getValorantWeapons(result.data.Guns, lang).then((result) => {
+        weapons.push(result);
+      });
+      await getValorantSprays(result.data.Sprays, lang).then((result) => {
+        sprays.push(result);
+      });
       res.status(200).json({
         status: "success",
-        data: result.data
+        data: {
+          weapons: weapons[0],
+          sprays: sprays[0]
+        }
       });
     })
     .catch((err) => {
